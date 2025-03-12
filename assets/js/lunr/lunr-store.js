63f8d61f6adf46e4980e0ec413f360eb6e5b5eb6
tgamblin <tgamblin@users.noreply.github.com>
2025-03-12 23:28:52 +0000

    jekyll build from Action 243b2ed126f07b711baf7f52640c4d586a68fa51

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,139 @@
+var store = [{
+        "title": "Spack on AWS cluster",
+        "excerpt":"Deploying an HPC cluster on Amazon Web Services might sound daunting, but it’s possible with Spack. Harvard’s Jiawei Zhuang has written a guide for standing up a cloud-HPC cluster using AWS ParallelCluster, Slurm, and Spack. The guide provides an easy-to-follow workflow using the Weather Research and Forecasting model as an...","categories": [],
+        "tags": ["cloud","hpc"],
+        "url": "/spack-aws-cluster/",
+        "teaser": ""
+      },{
+        "title": "Spack has a new website",
+        "excerpt":"Until now, spack.io has mostly been a landing page – an easy-to-remember URL with a brief description of Spack and links out to other, more important project pages. The old spack.io was great as a splash page, but its single-page layout was a bit cramped for all the stuff going...","categories": [],
+        "tags": ["spack.io","website"],
+        "url": "/spack-has-a-new-website/",
+        "teaser": ""
+      },{
+        "title": "Spack at SC19",
+        "excerpt":"Supercomputing 2019 (SC19) kicks off this week in Denver, and the Spack team will be busy with a tutorial, several BOFs, two meet-the-developers sessions at the DOE booth, and even some papers! See below for a list of events by us and our collaborators. Be sure to follow @spackpm on...","categories": [],
+        "tags": ["sc19","tutorial","bof","workshop","talks","events"],
+        "url": "/spack-at-sc19/",
+        "teaser": ""
+      },{
+        "title": "Spack wins FLC award",
+        "excerpt":"We’re excited to announce that Spack received a regional technology transfer award from the Federal Laboratory Consortium (FLC). The FLC has been around since 1974 and recognizes federal labs and their industry partners for outstanding tech transfer achievements. Thanks to the global Spack community for helping us make Spack what...","categories": [],
+        "tags": ["award","flc"],
+        "url": "/spack-wins-flc-award/",
+        "teaser": ""
+      },{
+        "title": "Spack wins R&D 100 award",
+        "excerpt":"The trade journal R&amp;D World Magazine announced the winners of its annual awards on October 31. And Spack is one of the winners! The competition recognizes top scientific and engineering products and tech with commercial potential. We also received a silver medal in the Market Disruptor category. Check out the...","categories": [],
+        "tags": ["award","rd100","video"],
+        "url": "/spack-wins-rd-100-award/",
+        "teaser": ""
+      },{
+        "title": "Spack on Microsoft Azure VMs",
+        "excerpt":"Another HPC cloud use case! A Microsoft Tech Community blog post describes the steps for using Spack to build and install packages on the Azure cloud for HPC workloads. There’s a repo you can fork to try it yourself: azurehpc contains scripts to automatically install Spack on Azure VMs, set...","categories": [],
+        "tags": ["cloud","hpc","microsoft"],
+        "url": "/spack-on-microsoft-azure-vms/",
+        "teaser": ""
+      },{
+        "title": "Video: Spack at FOSDEM '20",
+        "excerpt":"FOSDEM promotes the widespread use of free and open source software. The 2020 conference took place in Brussels, Belgium, on February 1–2. The FOSDEM website is chock full of videos of speakers, lightning talks, and other sessions. Todd Gamblin led two Spack sessions: Spack’s new Concretizer: Dependency solving is more...","categories": [],
+        "tags": ["fosdem","video","events","talks"],
+        "url": "/spack-at-fosdem/",
+        "teaser": ""
+      },{
+        "title": "Spack at ECP annual meeting",
+        "excerpt":"We’re thrilled to be part of the Exascale Computing Project (ECP), a joint effort between the DOE Office of Science and NNSA that brings several together national labs. These combined forces tackle the hardware, software, and application challenges inherent in the DOE/NNSA’s scientific and national security missions. Spack is the...","categories": [],
+        "tags": ["ecp","events","hpc"],
+        "url": "/spack-at-ecp-annual-meeting/",
+        "teaser": ""
+      },{
+        "title": "Exascale watch: El Capitan",
+        "excerpt":"HPC centers are always prepping for the next big thing. LLNL’s El Capitan supercomputer, coming online in 2023, will help usher in the exascale era with its peak expected performance of 2 exaflops. In this HPCwire article, LLNL’s CTO Bronis de Supinski describes readying software deployment for El Capitan –...","categories": [],
+        "tags": ["elcapitan","hpc"],
+        "url": "/exascale-watch-el-capitan/",
+        "teaser": ""
+      },{
+        "title": "Into a new decade: using Spack at LRZ",
+        "excerpt":"At the start of the new decade, it is the right time to reflect on the use of Spack at the Leibniz Supercomputing Centre (LRZ). We’d like to take a look at the current developments and pull out the crystal ball for a glimpse into the (near) future. The story...","categories": [],
+        "tags": ["users","lrz","supermuc-ng","hpc"],
+        "url": "/lrz-using-spack/",
+        "teaser": ""
+      },{
+        "title": "Profile of Greg Becker",
+        "excerpt":"Spack contributor Greg Becker has many talents, not the least of which is scaling rock faces. LLNL Computing profiled this computer scientist in a piece titled “Greg Becker Scales Projects and Mountains.” Greg is a member of core the Spack development team – and easily reachable on Spack Slack (becker33)...","categories": [],
+        "tags": ["people"],
+        "url": "/greg-becker-profile/",
+        "teaser": ""
+      },{
+        "title": "Podcast: Let's Talk Exascale",
+        "excerpt":"The Exascale Computing Project’s Let’s Talk Exascale podcast has a new episode featuring Spack: “Flexible Package Manager Automates the Deployment of Software on Supercomputers”. Todd Gamblin describes Spack’s role in the ECP’s software stack, the Spack-related events at SC19, and the R&amp;D 100 Award. The converation covers what it’s like...","categories": [],
+        "tags": ["exascale","podcast","ecp"],
+        "url": "/ecp-podcast-episode-67/",
+        "teaser": ""
+      },{
+        "title": "Working remotely: the Spack team",
+        "excerpt":"With much of the tech sphere working from home right now, a Better Scientific Software blog post describes the Spack team’s experience working remotely. The Spack team has had to make some adjustments without being able to hold face-to-face tutorials and attend events in person, but there are a lot...","categories": [],
+        "tags": ["bssw"],
+        "url": "/bssw-working-remotely/",
+        "teaser": ""
+      },{
+        "title": "Spack R&D 100 award featured in LLNL magazine",
+        "excerpt":"The July issue of Lawrence Livermore’s magazine Science &amp; Technology Review features Spack as one of the Lab’s four winning technologies from 2019. The article titled “Software Installation Simplified” covers Spack’s origins, mechanisms, key features, and thriving open source community. The magazine issue also has a commentary by LLNL’s deputy...","categories": [],
+        "tags": ["award","rd100"],
+        "url": "/spack-rd-100-award-featured-llnl-magazine/",
+        "teaser": ""
+      },{
+        "title": "Spack at SC20",
+        "excerpt":"Supercomputing 2020 (SC20) runs for two weeks beginning on November 9. See below for a list of our events. Be sure to follow @spackpm on Twitter for updates! Mon., November 9 10:00am - 2:00pm EST (live) Our tutorial runs over two half-days. Managing HPC Software Complexity with Spack: Part 1...","categories": [],
+        "tags": ["sc20","tutorial","bof","events"],
+        "url": "/spack-at-sc20/",
+        "teaser": ""
+      },{
+        "title": "Spack featured on The Next Platform",
+        "excerpt":"The Next Platform provides in-depth coverage of high-end computing at large enterprises, supercomputing centers, hyperscale data centers, and public clouds. Against the backdrop of the Supercomputing (SC20) conference, the article titled “Spack Packs Deployment Boost for Top Supercomputers” describes the challenges of creating and deploying scientific software packages. Writer Nicole...","categories": [],
+        "tags": ["next-platform"],
+        "url": "/spack-featured-next-platform/",
+        "teaser": ""
+      },{
+        "title": "Spack User Survey 2020",
+        "excerpt":"Introduction Spack has been around for a while, and we’ve always felt like we have a pretty good sense of the community through channels like GitHub, Slack, and our Google group. However, the project is getting larger, and we wanted to better understand the community’s needs with more structured feedback....","categories": [],
+        "tags": ["user","survey","2020","community"],
+        "url": "/spack-user-survey-2020/",
+        "teaser": ""
+      },{
+        "title": "ECP annual meeting videos now available",
+        "excerpt":"Spack is the software deployment tool of the Exascale Computing Project (ECP), a joint effort between the DOE Office of Science and NNSA that brings several together national labs. These combined forces tackle the hardware, software, and application challenges inherent in the DOE/NNSA’s scientific and national security missions. The ECP’s...","categories": [],
+        "tags": ["exascale","video","ecp","tutorial","hpc"],
+        "url": "/ecp-videos/",
+        "teaser": ""
+      },{
+        "title": "Podcast: Spack on CppCast",
+        "excerpt":"The CppCast podcast recently hosted Spack creator Todd Gamblin and core developer Greg Becker. They discuss a range of topics including an overview of the exascale computing landscape, documentation generators, floating-point numbers and performance standards, a new project focusing on ABI (application binary interface) compatibility, getting the most out of...","categories": [],
+        "tags": ["podcast","cpp"],
+        "url": "/cppcast-podcast/",
+        "teaser": ""
+      },{
+        "title": "Changes to bootstrapping in Spack v0.17",
+        "excerpt":"Starting with Spack v0.17, the new concretizer will be the default, and Spack will automatically install a new dependency (Clingo) from binaries. You can optionally disable this bootstrapping. Spack and its own dependencies Since its earliest releases, Spack has had software requirements, and we’ve tried to restrict them to very...","categories": [],
+        "tags": ["release","bootstrapping","v0.17"],
+        "url": "/changes-spack-v017/",
+        "teaser": ""
+      },{
+        "title": "Announcing public binaries for Spack",
+        "excerpt":"Spack was designed as a from-source package manager, but today users can stop waiting for builds. Spack has been able to create binary build caches for several years, but the onus of building the binaries has been on users: application teams, deployment teams at HPC facilities, etc. Today, enabled by...","categories": [],
+        "tags": ["public","binary","cache","aws","kitware","e4s","v0.18"],
+        "url": "/spack-binary-packages/",
+        "teaser": ""
+      },{
+        "title": "Join us for the first Spack User Meeting!",
+        "excerpt":"We hope you can join us for the first ever Spack User Meeting (SUM) in Chicago, IL on May 7th and 8th, 2025, as part of the first High Performance Software Foundation (HPSFcon) conference. What to Expect Updates from Spack developers We’ll give a talk on the State of the...","categories": [],
+        "tags": ["SUM","spack","user","group","meeting","hpsf","conference","chicago"],
+        "url": "/spack-user-meeting-2025/",
+        "teaser": ""
+      },{
+        "title": "The schedule is live for the first Spack User Meeting!",
+        "excerpt":"We’ve been meaning to do this for years, and it feels great to announce the schedule for the very first Spack User Meeting, SUM 2025, colocated with HPSFCon 2025. We have 34 contributed talks lined up from users and contributors all over the world. Schedule The full schedule is online...","categories": [],
+        "tags": ["SUM","spack","user","group","meeting","hpsf","conference","chicago","schedule"],
+        "url": "/sum25-schedule/",
+        "teaser": ""
+      }]
